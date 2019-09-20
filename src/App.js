de6b003f95379f4b0dbbe0180b7cb93a0cdb49da
Birdevinder Singh <Veer davinder>
2019-09-20 14:38:30 -0400

    Getting Started with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import Table from './Table'
+import Form from './Form'
+class App extends Component{
+  state={
+    characters: []
+  }
+    render(){
+        return(
+            <div className="container">
+              <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
+              <Form handleSubmit={this.handleSubmit}/>
+            </div>
+        )
+    }
+ removeCharacter = index =>{
+  const characters = this.state.characters;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  this.setState({
+    characters:characters.filter((character,i)=>{
+      return i !==index
+    })
+  })
 }
 
-export default App;
+handleSubmit = character =>{
+  this.setState({characters:[...this.state.characters,character]})
+}
+}
+export default App
